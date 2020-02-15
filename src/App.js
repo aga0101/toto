e3e40df8f8f18f55eb3a97b780a1fb5f16c8a96d
Aga Dwulit <aga.i.dwulit@gmail.com>
2020-02-15 18:42:44 -0500

    + v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar.js';
+import Diagram from './components/Diagram.js';
+import Form from './components/Form.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <Navbar />
+      <header className="masthead bg-primary text-white text-center">
+        <div class="container d-flex align-items-center flex-column">
+          <img class="img-responsive" width="233" height="300" src="img/toto.png" alt=""/>
+          <h1 class="masthead-heading text-uppercase mb-0 text-white text-center">Shop &amp; Adopt</h1>
+          <div class="divider-custom divider-dark">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon">
+              <i class="fas fa-star"></i>
+            </div>
+            <div class="divider-custom-line"></div>
+          </div>
+          <p class="masthead-subheading font-weight-light mb-0 ">Help place more shelter pets in their forever homes.</p>
+        </div>
       </header>
+      <Diagram />
+      <Form />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
