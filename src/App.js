4c2e0c7617216efcc29819cb12fc6fd207549e6b
Aga Dwulit <aga.i.dwulit@gmail.com>
2020-03-10 14:21:08 -0400

    fixed routing issues and implemented social login feature

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
-import Diagram from './components/Diagram.js';
-import Form from './components/Form.js';
 import Footer from './components/Footer.js';
+import { useAuth0 } from "./react-auth0-spa";
+import { Router, Route, Switch } from "react-router";
+import Profile from "./components/Profile";
+import history from "./utils/history";
 
 function App() {
+  
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <Navbar />
-      <header className="masthead bg-primary text-white text-center">
-        <div class="container d-flex align-items-center flex-column">
-          <br/>
-          <br/>
-          <div class="frame">
-          <img class="img-responsive" width="200" height="200" src="img/totologo.png" alt=""/>
-          </div>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <h1 class="masthead-heading text-uppercase mb-0 text-white text-center">Shop To Adopt</h1>
-          <div class="divider-custom divider-dark">
-            <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon">
-              <i class="fas fa-paw"></i>
+      <Router history={history}>
+        <Navbar />
+        
+        <header className="masthead bg-primary text-white text-center">
+          <div class="container d-flex align-items-center flex-column">
+            <br/>
+            <br/>
+            <div class="frame">
+            <img class="img-responsive" width="200" height="200" src="img/totologo.png" alt=""/>
             </div>
-            <div class="divider-custom-line"></div>
+            <br/>
+            <br/>
+            <br/>
+            <h1 class="masthead-heading text-uppercase mb-0 text-white text-center">Shop To Adopt</h1>
+            <div class="divider-custom divider-dark">
+              <div class="divider-custom-line"></div>
+              <div class="divider-custom-icon">
+                <i class="fas fa-paw"></i>
+              </div>
+              <div class="divider-custom-line"></div>
+            </div>
+            <p class="masthead-subheading font-weight-light mb-0 ">Help place more shelter pets in their forever homes.</p>
           </div>
-          <p class="masthead-subheading font-weight-light mb-0 ">Help place more shelter pets in their forever homes.</p>
-        </div>
-      </header>
-      <Diagram />
-      <Form />
-      <Footer />
+        </header>
+        <Switch>
+        <Route path="/" exact />
+        <Route path="/profile" component={Profile} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
+
 
 export default App;
 
